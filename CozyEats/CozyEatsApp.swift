cf5fb7422e890255d11981b3dad16a9ae5ed32d0
Benjamin <bmelville8@gatech.edu>
2023-10-05 18:44:26 -0400

    Initial Commit

---
+++ CozyEats/CozyEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CozyEatsApp.swift
+//  CozyEats
+//
+//  Created by Benjamin Melville on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CozyEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
