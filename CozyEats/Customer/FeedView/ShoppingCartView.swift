12f60b979b44e561e5468102bdc5216022932e28
Benjamin <bmelville8@gatech.edu>
2023-11-10 17:16:31 -0500

    added shopping cart front-end and backend.

---
+++ CozyEats/Customer/FeedView/ShoppingCartView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingCartView.swift
+//  CozyEats
+//
+//  Created by Benjamin Melville on 11/10/23.
+//
+
+import SwiftUI
+
+struct ShoppingCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingCartView()
+}
